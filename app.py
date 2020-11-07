fa45dc353d77e4d77fe8cb32ae49186af010d49b
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2020-11-07 05:32:08 +0530

    Updated with db connection

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ ENV = 'dev'
 
 if ENV == 'dev':
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/gorail2'
 else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = ''

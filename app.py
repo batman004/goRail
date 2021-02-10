6b8796557154e1f850fbcf0e4e2426eb803b8841
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2021-02-10 16:00:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,9 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = settings.DEVELOPMENT_DATABASE_URI
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = settings.PRODUCTION_DATABASE_URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cwdxrtpyxksiyu:' \
+    '9b4ec83827aaeda7bdf2740a22cd03742caf3d94835745ce711226274ede18d7' \
+    '@ec2-34-237-166-54.compute-1.amazonaws.com:5432/dmq6enggu0l4j'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
